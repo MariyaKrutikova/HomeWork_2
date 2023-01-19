2941666c618c7a1dba724453c8bd52f89ec702d0
Mariya <appleslover@yandex.ru>
2023-01-19 23:37:31 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetNewNumber()
+{
+    Console.Write("Введите трехзначное число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PrintSecondNumber(int number)
+{
+    int a = number / 100;
+    int c = number % 10;
+    int b = (number - a *100 - c) / 10;
+    return b;
+}
+
+int number = GetNewNumber();
+Console.WriteLine(number);
+
+int NewNumber = PrintSecondNumber(number);
+Console.WriteLine(NewNumber);
+
